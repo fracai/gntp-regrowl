88a70282c19eaae464f7f2dda98579e4d2dc3dee
Arno Hautala <arno@alum.wpi.edu>
2013-11-24 00:50:38 -0500

    corrected forwarding to nma, prowl, pushover

--- regrowl/bridge/forward.py
+++ regrowl/bridge/forward.py
@@ -55,42 +55,49 @@ class GrowlForwarder(ReGrowler):
             return 0
 
     def forwardpackettodestination(self, packet, destination):
-            if destination[0] == "network":
-                logger.info("Forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination " + destination[1] + ":" + destination[2])
-                if self.validatedestination(packet,self.srcaddr,destination[1]):
-                    notifier = gntp.notifier.GrowlNotifier(hostname = destination[1], port = int(destination[2]), password = destination[3])
-                    packet.info['encryptionAlgorithmID'] = "NONE"
-                    packet.add_header(
-                        "Received", 
-                        "From %(source)s by %(receiver)s with Growl id %(identifier)s; %(date)s" % 
-                        {
-                            'source':packet.headers.get('Origin-Machine-Name'),
-                            'receiver':socket.gethostname(),
-                            'identifier':packet.message_id,
-                            'date':time.strftime("%Y-%m-%d %H:%M:%SZ",time.gmtime())
-                        }
-                        )
-                    if destination[3]:
-                        packet.set_password(destination[3],'MD5')
+        if destination[0] == "network":
+            logger.info("Forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination " + destination[1] + ":" + destination[2])
+            if self.validatedestination(packet,self.srcaddr,destination[1]):
+                notifier = gntp.notifier.GrowlNotifier(hostname = destination[1], port = int(destination[2]), password = destination[3])
+                packet.info['encryptionAlgorithmID'] = "NONE"
+                packet.add_header(
+                    "Received", 
+                    "From %(source)s by %(receiver)s with Growl id %(identifier)s; %(date)s" % 
+                    {
+                        'source':packet.headers.get('Origin-Machine-Name'),
+                        'receiver':socket.gethostname(),
+                        'identifier':packet.message_id,
+                        'date':time.strftime("%Y-%m-%d %H:%M:%SZ",time.gmtime())
+                    }
+                    )
+                if destination[3]:
+                    packet.set_password(destination[3],'MD5')
                     try:
                         notifier._send(packet.info['messagetype'],packet)
                     except Exception, e:
                         logger.info("Network error while forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination " + destination[1] + ":" + destination[2] + ":")
                         logger.info(e)
-            elif destination[0] in ["nma", "prowl", "pushover"]:
-                if provider_key[destination[0]]:
-                    logger.info("Forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination, API Key: " + destination[1])
-                    client = pushnotify.get_client(destination[0], developerkey=provider_key[destination[0]], application=packet.headers.get('Application-Name'))
-                    client.add_key(destination[1])
-                    try:
-                        client.notify(description = packet.headers.get('Notification-Text'), event = packet.headers.get('Notification-Title'))
-                    except Exception, e:
-                        logger.info("error while forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination, API Key: " + destination[1] + ":")
-                        logger.info(e)
-                else:
-                    logger.info("error while forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination, no developer key")
+        elif destination[0] in ["nma", "prowl", "pushover"]:
+            if len(destination) >= 3:
+                logger.error(destination[2])
+                token = destination[2]
+            elif provider_key[destination[0]]:
+                token = provider_key[destination[0]]
             else:
-                logger.error("Invalid forwarding destination type for message [" + str(packet.message_id) + "]: " + destination[0])
+                token = None
+            if token:
+                logger.info("Forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination, API Key: " + destination[1])
+                client = pushnotify.get_client(destination[0], developerkey=token, application=packet.headers.get('Application-Name'))
+                client.add_key(destination[1])
+                try:
+                    client.notify(description = packet.headers.get('Notification-Text'), event = packet.headers.get('Notification-Title'))
+                except Exception, e:
+                    logger.error("error while forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination, API Key: " + api_key + ":")
+                    logger.error(e)
+            else:
+                logger.error("error while forwarding message [" + str(packet.message_id) + "] to " + destination[0] + " destination, no developer key")
+        else:
+            logger.error("Invalid forwarding destination type for message [" + str(packet.message_id) + "]: " + destination[0])
 
 
 
